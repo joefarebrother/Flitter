2d5b45a6703df1922d01a084a07cd73c64520f17
Joe Farebrother <nintendo.joe.farebrother@gmail.com>
2018-03-30 16:00:40 +0100

    Add frontend part of interface for algorithmsettings

--- public/javascripts/algorithmsettings.js
+++ public/javascripts/algorithmsettings.js
@@ -1,4 +1,24 @@
 
 $("#go").click(()=>{
-  //Refresh news feed
+	var data = {}
+	$(".slider").map((i, s) => {
+		data[$(s).attr("id")] = $(s).val() / $(s).attr("max")
+	})
+
+  $.ajax({
+  	url: "api/setAlgSettings",
+  	data: data,
+  	success: () => {
+  		$("#status")
+  			.text("Successfully updated settings")	
+  			.css("background", "#53c68c")
+  			.fadeIn()
+  	},
+  	error: () => {
+  		$("#status")
+  			.text("An error occoured")
+  			.css("background", "#ff3333")
+  			.fadeIn()
+  	},
+  })
 });
