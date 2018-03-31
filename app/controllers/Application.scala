3e681b48ccc814f4f4a4e4c16a6c891911e83458
joefarebrother <joseph.farebrother@gmail.com>
2018-04-01 00:32:42 +0100

    Finish getTweets (i.e. backend interface) (not tested) Fix tabs/spaces issues

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,5 +1,3 @@
-Skip to content
-
 package controllers
 
 import algorithms._
@@ -11,8 +9,10 @@ import play.api.Play.current
 
 import play.api.db._
 import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
-import java.time.LocalDateTime
+import java.time._
+import java.io._
 
 object Application extends Controller {
 
@@ -26,54 +26,52 @@ object Application extends Controller {
 	}
 
 
-  def index = Action {
-    Ok(views.html.index())
-  }
+	def index = Action {
+	  Ok(views.html.index())
+	}
 
-  def usersettings = Action {
-  	val user_handle = "user1" // placeholder
-  	DB.withConnection{conn =>
-  		Ok(views.html.usersettings(
-  			getUserWithFollowing(conn, getUserByHandle(conn, user_handle))))
-  	}
-    
-  }
+	def usersettings = Action {
+		val user_handle = "user1" // placeholder
+		DB.withConnection{conn =>
+			Ok(views.html.usersettings(
+				getUserWithFollowing(conn, getUserByHandle(conn, user_handle))))
+		}
+	}
 
-  def algorithmsettings = Action {
-  	val user_handle = "user1" // placeholder
-  	DB.withConnection{conn =>
-  		Ok(views.html.algorithmsettings(
-  			getWeighting(conn, getUserByHandle(conn, user_handle))))
-  	}
-    
-  }
+	def algorithmsettings = Action {
+		val user_handle = "user1" // placeholder
+		DB.withConnection{conn =>
+			Ok(views.html.algorithmsettings(
+				getWeighting(conn, getUserByHandle(conn, user_handle))))
+		}
+	}
 
-  def followUser = updateFollows("INSERT INTO usersFollowing (uid, following) VALUES (?, ?)")
+	def followUser = updateFollows("INSERT INTO usersFollowing (uid, following) VALUES (?, ?)")
 
-  def unfollowUser = updateFollows("DELETE FROM usersFollowing WHERE uid = ? AND following = ?")
+	def unfollowUser = updateFollows("DELETE FROM usersFollowing WHERE uid = ? AND following = ?")
 
-  def followHashtag = updateFollows("INSERT INTO hashtagsFollowing (uid, following) VALUES (?, ?)")
+	def followHashtag = updateFollows("INSERT INTO hashtagsFollowing (uid, following) VALUES (?, ?)")
 
 	def unfollowHashtag = updateFollows("DELETE FROM hashtagsFollowing WHERE uid = ? AND following = ?")
 
 
 
-  def updateFollows(query: String) = Action {req =>
-  	val user_handle = "user1" // placeholder
-  	DB.withConnection{conn =>
-  		val user = getUserByHandle(conn, user_handle)
-  		val pstmt = conn.prepareStatement(query)
-  		pstmt.setInt(1, user.id)
-  		pstmt.setString(2, req.getQueryString("name").getOrElse(""))
-  		pstmt.executeUpdate()
-  	}
-  	Ok("")
-  }
+	def updateFollows(query: String) = Action {req =>
+		val user_handle = "user1" // placeholder
+		DB.withConnection{conn =>
+			val user = getUserByHandle(conn, user_handle)
+			val pstmt = conn.prepareStatement(query)
+			pstmt.setInt(1, user.id)
+			pstmt.setString(2, req.getQueryString("name").getOrElse(""))
+			pstmt.executeUpdate()
+		}
+		Ok("")
+	}
 
-  
 
-  def setAlgSettings = Action { req =>
-  	def getParam(key: String) = {
+
+	def setAlgSettings = Action { req =>
+		def getParam(key: String) = {
 			req.getQueryString(key) match {
 				case None => 1f
 				case Some(str) => try { 
@@ -84,180 +82,159 @@ object Application extends Controller {
 			}
 		}
 
-  	val user_handle = "user1" // placeholder
-  	DB.withConnection { conn =>
-  		val pstmt = conn.prepareStatement(
-  			"UPDATE users SET "
-  			+ "setting_proximity = ?,"
-  			+ "setting_timeliness = ?,"
-  			+ "setting_hashtags = ?,"
-  			+ "setting_popularity = ?"
-  			+ "WHERE handle = ?")
-  		pstmt.setFloat(1, getParam("proximity"))
-  		pstmt.setFloat(2, getParam("timeliness"))
-  		pstmt.setFloat(3, getParam("hashtags"))
-  		pstmt.setFloat(4, getParam("popularity"))
-  		pstmt.setString(5, user_handle)
-  		pstmt.executeUpdate()
-  	}
+		val user_handle = "user1" // placeholder
+		DB.withConnection { conn =>
+			val pstmt = conn.prepareStatement(
+				"UPDATE users SET "
+				+ "setting_proximity = ?,"
+				+ "setting_timeliness = ?,"
+				+ "setting_hashtags = ?,"
+				+ "setting_popularity = ?"
+				+ "WHERE handle = ?")
+			pstmt.setFloat(1, getParam("proximity"))
+			pstmt.setFloat(2, getParam("timeliness"))
+			pstmt.setFloat(3, getParam("hashtags"))
+			pstmt.setFloat(4, getParam("popularity"))
+			pstmt.setString(5, user_handle)
+			pstmt.executeUpdate()
+		}
 
-  	// could update scores for each tweet here
+		// could update scores for each tweet here
 
-  	Ok("")
-  		
-  }
+		Ok("")
+			
+	}
 
-  def getTweets = Action {
-	val fileName = //need file name of json (I dont know)
-  	val stream = new FileInputStream(new File(fileName))
-	val json = try { Json.parse(stream) } finally { stream.close() }
-	case class Tweet(id: Int, 
-                   user:String,
-                   timestamp: java.time.LocalDateTime, 
-                   hashtags: List[String], 
-                   text: String, 
-                   location: Location, 
-                   retweets: Int, 
-                   favourites: Int)
-	object Tweet {
-		implicit val jsonReadsL Reads[Tweet] = (
+	def getTweets = Action {
+		val filepath = "Backend/data_formatted.json"
+		val stream = new FileInputStream(new File(filepath))
+		val json = try { Json.parse(stream) } finally { stream.close() }
+		def coordsToLoc(c: Option[List[Float]]): algorithms.Location = {
+			c match {
+				case Some(List(lat, long)) => new algorithms.Location(lat=lat, long=long)
+				case _  => algorithms.Location(lat=0, long=0) // default              
+			}		
+		}
+		def timestampToLocalDateTime(ms: Int) = {
+			LocalDateTime.ofInstant(Instant.ofEpochMilli(ms), 
+                                java.util.TimeZone.getDefault().toZoneId())
+		}
+		implicit val jsonReadsL: Reads[algorithms.Tweet] = (
 			(JsPath \ "id").read[Int] and
-			(JsPath \ "user" \ "name").read[String] and
-			(JsPath \ "created_at").read[String] and
+			(JsPath \ "user" \ "handle").read[String] and
+			(JsPath \ "timestamp_ms").read[Int].map(timestampToLocalDateTime _) and
 			(JsPath \ "entities" \ "hashtags").read[List[String]] and
-			(JsPath \ "geo").read[Location] and
+			(JsPath \ "text").read[String] and
+			(JsPath \\ "coordinates").readNullable[List[Float]].map(coordsToLoc _) and
 			(JsPath \ "retweet_count").read[Int] and
-			(JsPath \ "favorite_count").read[Int])(Tweet.apply _)
-	}	
-	val result = json.as[Seq[Tweet]]
-  	val the_tweets = List(
-  		new algorithms.Tweet(
-  			id=1,
-  			user="placeholder",
-  			timestamp=java.time.LocalDateTime.now(),
-  			hashtags=List("test", "changeme"),
-  			text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. #test #changeme",
-  			location=new algorithms.Location(
-  				lat= 51.752022f,
-  				long= -1.257677f), //Oxford
-  			retweets=420,
-  			favourites=69
-  		),
-  		new algorithms.Tweet(
-  			id=2,
-  			user="RickAstley",
-  			timestamp=java.time.LocalDateTime.now(),
-  			hashtags=List("rickroll"),
-  			text="Never gonna give you up! #rickroll",
-  			location = new algorithms.Location(lat=0, long=0),
-  			retweets=1337,
-  			favourites=42
-  		)
-  	)
-	the_tweets = result ++ the_tweets //This might not be the correct syntax
+			(JsPath \ "favorite_count").read[Int]
+		)(algorithms.Tweet.apply _)
+			
 
-  	val user_handle = "user1"
-  	DB.withConnection { conn =>
-  		val user = getUserWithFollowing(conn, getUserByHandle(conn, user_handle))
-  		val weights = getWeighting(conn, user)
-  		val sorted = algorithms.sort(the_tweets, user, weights)
+		val the_tweets = json.as[List[algorithms.Tweet]]
 
-  		Ok(Json.toJson(sorted.take(100))) 	
-  	}
-  }
+		val user_handle = "user1"
+		DB.withConnection { conn =>
+			val user = getUserWithFollowing(conn, getUserByHandle(conn, user_handle))
+			val weights = getWeighting(conn, user)
+			val sorted = algorithms.sort(the_tweets, user, weights)
 
-  def getUserByHandle(conn: java.sql.Connection, handle: String) = {
-  	val stmt = conn.createStatement
-  	stmt.executeUpdate(
-  		"CREATE TABLE IF NOT EXISTS users ("
-  	  + "id SERIAL,"
-  	  + "name varchar DEFAULT '',"
-      + "handle varchar,"
-      + "lat float DEFAULT 51.752022,"
-      + "long float DEFAULT -1.257677," // default is Oxford
-      + "setting_proximity float DEFAULT 1,"
-      + "setting_timeliness float DEFAULT 1,"
-      + "setting_hashtags float DEFAULT 1,"
-      + "setting_popularity float DEFAULT 1,"
-      + "setinng_userrelation float DEFAULT 1,"
-      + "PRIMARY KEY(id))");
+			Ok(Json.toJson(sorted.take(100))) 	
+		}
+	}
 
-  	var pstmt = conn.prepareStatement("SELECT * FROM users WHERE handle = ?")
-  	pstmt.setString(1, handle)
-  	var rs = pstmt.executeQuery()
-  	if (!rs.next){
-  		pstmt = conn.prepareStatement("INSERT INTO users (handle) VALUES(?)")
-  		pstmt.setString(1, handle)
-  		pstmt.executeUpdate()
+	def getUserByHandle(conn: java.sql.Connection, handle: String) = {
+		val stmt = conn.createStatement
+		stmt.executeUpdate(
+			"CREATE TABLE IF NOT EXISTS users ("
+			+ "id SERIAL,"
+			+ "name varchar DEFAULT '',"
+			+ "handle varchar,"
+			+ "lat float DEFAULT 51.752022,"
+			+ "long float DEFAULT -1.257677," // default is Oxford
+			+ "setting_proximity float DEFAULT 1,"
+			+ "setting_timeliness float DEFAULT 1,"
+			+ "setting_hashtags float DEFAULT 1,"
+			+ "setting_popularity float DEFAULT 1,"
+			+ "setinng_userrelation float DEFAULT 1,"
+			+ "PRIMARY KEY(id))");
 
-  		pstmt = conn.prepareStatement("SELECT * FROM users WHERE handle = ?")
-  		pstmt.setString(1, handle)
-  		rs = pstmt.executeQuery()
-  		assert(rs.next)
-  	}
+		var pstmt = conn.prepareStatement("SELECT * FROM users WHERE handle = ?")
+		pstmt.setString(1, handle)
+		var rs = pstmt.executeQuery()
+		if (!rs.next){
+			pstmt = conn.prepareStatement("INSERT INTO users (handle) VALUES(?)")
+			pstmt.setString(1, handle)
+			pstmt.executeUpdate()
 
-  	new algorithms.User(
-  		id=rs.getInt("id"),
-  		name=rs.getString("name"),
-  		handle=handle,
-  		location=new algorithms.Location(
-  			lat=rs.getFloat("lat"),
-  			long=rs.getFloat("long")
-  		),
-  		usersFollowing=List(),
-  		hashtagsFollowing=List()
-  	)
-  }
+			pstmt = conn.prepareStatement("SELECT * FROM users WHERE handle = ?")
+			pstmt.setString(1, handle)
+			rs = pstmt.executeQuery()
+			assert(rs.next)
+		}
 
-  def getWeighting(conn: java.sql.Connection, user: algorithms.User) = {
-  	val pstmt = conn.prepareStatement("SELECT * FROM users WHERE id = ?")
-  	pstmt.setInt(1, user.id)
-  	val rs = pstmt.executeQuery()
-  	assert(rs.next)
-  	new algorithms.Weighting(
-  		proximity=rs.getFloat("setting_proximity"),
-  		timeliness=rs.getFloat("setting_timeliness"),
-  		hashtags=rs.getFloat("setting_hashtags"),
-  		popularity=rs.getFloat("setting_popularity")
-  	)
-  }
+		new algorithms.User(
+			id=rs.getInt("id"),
+			name=rs.getString("name"),
+			handle=handle,
+			location=new algorithms.Location(
+				lat=rs.getFloat("lat"),
+				long=rs.getFloat("long")
+			),
+			usersFollowing=List(),
+			hashtagsFollowing=List()
+		)
+	}
 
-  def getUserWithFollowing(conn: java.sql.Connection, user: algorithms.User) = {
-  	createFollowingTables(conn)
-  	var pstmt = conn.prepareStatement("SELECT * FROM usersFollowing WHERE uid = ?")
-  	pstmt.setInt(1, user.id)
-  	var rs = pstmt.executeQuery()
-  	var res1 = List[String]()
-  	while(rs.next){
-  		res1 = rs.getString("following") :: res1
-  	}
-  	
-  	pstmt = conn.prepareStatement("SELECT * FROM hashtagsFollowing WHERE uid = ?")
-  	pstmt.setInt(1, user.id)
-  	rs = pstmt.executeQuery()
-  	var res2 = List[String]()
-  	while(rs.next){
-  		res2 = rs.getString("following") :: res2
-  	}
+	def getWeighting(conn: java.sql.Connection, user: algorithms.User) = {
+		val pstmt = conn.prepareStatement("SELECT * FROM users WHERE id = ?")
+		pstmt.setInt(1, user.id)
+		val rs = pstmt.executeQuery()
+		assert(rs.next)
+		new algorithms.Weighting(
+			proximity=rs.getFloat("setting_proximity"),
+			timeliness=rs.getFloat("setting_timeliness"),
+			hashtags=rs.getFloat("setting_hashtags"),
+			popularity=rs.getFloat("setting_popularity")
+		)
+	}
 
-  	user.copy(usersFollowing=res1, hashtagsFollowing=res2)
+	def getUserWithFollowing(conn: java.sql.Connection, user: algorithms.User) = {
+		createFollowingTables(conn)
+		var pstmt = conn.prepareStatement("SELECT * FROM usersFollowing WHERE uid = ?")
+		pstmt.setInt(1, user.id)
+		var rs = pstmt.executeQuery()
+		var res1 = List[String]()
+		while(rs.next){
+			res1 = rs.getString("following") :: res1
+		}
+		
+		pstmt = conn.prepareStatement("SELECT * FROM hashtagsFollowing WHERE uid = ?")
+		pstmt.setInt(1, user.id)
+		rs = pstmt.executeQuery()
+		var res2 = List[String]()
+		while(rs.next){
+			res2 = rs.getString("following") :: res2
+		}
 
-  }
+		user.copy(usersFollowing=res1, hashtagsFollowing=res2)
 
-  def createFollowingTables(conn: java.sql.Connection) = {
-  	val stmt = conn.createStatement
-  	stmt.executeUpdate(
-  		"CREATE TABLE IF NOT EXISTS usersFollowing ("
-  		+ "id SERIAL,"
-  		+ "uid integer REFERENCES users(id),"
-  		+ "following varchar,"
-  		+ "PRIMARY KEY(id))")
-  	stmt.executeUpdate(
-  		"CREATE TABLE IF NOT EXISTS hashtagsFollowing ("
-  		+ "id SERIAL,"
-  		+ "uid integer REFERENCES users(id),"
-  		+ "following varchar,"
-  		+ "PRIMARY KEY(id))")
-  }
+	}
+
+	def createFollowingTables(conn: java.sql.Connection) = {
+		val stmt = conn.createStatement
+		stmt.executeUpdate(
+			"CREATE TABLE IF NOT EXISTS usersFollowing ("
+			+ "id SERIAL,"
+			+ "uid integer REFERENCES users(id),"
+			+ "following varchar,"
+			+ "PRIMARY KEY(id))")
+		stmt.executeUpdate(
+			"CREATE TABLE IF NOT EXISTS hashtagsFollowing ("
+			+ "id SERIAL,"
+			+ "uid integer REFERENCES users(id),"
+			+ "following varchar,"
+			+ "PRIMARY KEY(id))")
+	}
 }
 
