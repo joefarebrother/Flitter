9120f0e8ea70edbe45920aa248c0c04394ae7408
cs-grant <36478203+cs-grant@users.noreply.github.com>
2018-04-05 20:17:21 +0100

    Update newsfeed.js
    Altered DislayTweet to round the score to 2dp, and to display the age of the tweet in a user-friendly format.

--- public/javascripts/newsfeed.js
+++ public/javascripts/newsfeed.js
@@ -2,11 +2,38 @@
 function displayTweet(tweet)
 // Takes a tweet object and adds the tweet to the bottom of the news feed
 {
+  // Round score to 2dp
+  tweet.score = Math.round(tweet.score * 100)/100;
+  
+  // Calculate age from timestamp
+  var timestamp = new Date(tweet.timestamp);
+  var age = new Date() - timestamp;
+  var ageString = '???';
+  age /= 1000; // Convert to seconds
+  if (age < 60) ageString = Math.round(age) + 's';
+  else {
+    age /= 60; // Convert to minutes
+    if (age < 60) ageString = Math.round(age) + 'm';
+    else {
+      age /= 60; //Convert to hours
+      if (age < 24) ageString = Math.round(age) + 'h';
+      else {
+        age /= 24; //Convert to days
+        if (age < 7) ageString = Math.round(age) + 'd';
+        else {
+          age /= 7;
+          ageString = Math.round(age) + 'w';
+        }
+      }
+    }
+  }
+  
+  // Display tweet
   var header = // user picture could go here
       "<p>" /*<span class='name'>" + tweet.user.name + "</span> &nbsp;"*/ +
       "<span class='handle'>" + tweet.user + "</span> &nbsp;&nbsp;" +
       "<span class='relevance'><i class='glyphicon glyphicon-stats'></i> " + tweet.score + "</span> &nbsp;" +
-      "<span class='age'><i class='glyphicon glyphicon-time'></i> " + tweet.timestamp + "</span></p>"; // should caclulate age
+      "<span class='age'><i class='glyphicon glyphicon-time'></i> " + ageString + "</span></p>";
   var content = "<div class='content'>" +
       "<p class='text'>" + tweet.text + "</p>" +
       // tweet image could go here
