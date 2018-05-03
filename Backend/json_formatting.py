5ddba5949fce79dc9ffbf21a5c6675daf80f9e03
Joe Farebrother <nintendo.joe.farebrother@gmail.com>
2018-05-03 11:52:28 +0100

    Fix issues with the data and put it into a format consistent with newly generated data

--- Backend/json_formatting.py
+++ Backend/json_formatting.py
@@ -13,10 +13,13 @@ import json
 filepath = './data.json'
 
 if __name__ == '__main__':
+
+    data = []
     # load the data 
     with open(filepath) as f:
-        data = json.load(f)
+      for line in f:
+        data.append(json.loads(line))
     
     # save the data with indenting
     with open('./data_formatted.json', 'w+') as f:
-        json.dump(data, f, indent=4)
+      json.dump(data, f, indent=4)
